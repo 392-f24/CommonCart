ac3d39d83797b2b6521997983ebd9e9e2dc23e70
ZiyeW-zz <125829074+ZiyeW-zz@users.noreply.github.com>
2024-10-22 00:36:14 -0500

    Navigation functionality

---
+++ src/components/pages/cartPage.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
+
+const CartPage = () => {
+    const navigate = useNavigate();
+  
+
+  
+    return (
+      <div className="cartPage">
+        <p> Cart Page</p>   
+        
+
+      </div>
+    );
+  };
+  
+  export default CartPage;
+  
