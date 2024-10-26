95a1162dd3ab32e05df3e2e47e907d6c8e8afde5
xuh5 <84805264+xuh5@users.noreply.github.com>
2024-10-26 10:13:50 -0500

    cart_page_UI_with_mock_data

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,20 +1,45 @@
 import { useNavigate } from 'react-router-dom';
-import './CartPage.css'
+import './CartPage.css';
 import { SignOut } from './SignIn';
+import CartItem from '../Components/CartItem';
 
 const CartPage = () => {
-    const navigate = useNavigate();
-  
+  // Example cart data
+  const carts = [
+    {
+      title: '2024 Roommates',
+      paymentType: 'Weekly Payment',
+      paymentDue: 'Next payment due: Monday, October 21',
+    },
+    {
+      title: 'Japan Summer Trip',
+      paymentType: 'One time Payment',
+      paymentDue: 'No payment due',
+    },
+  ];
 
-  
-    return (
-      <div className="cart-page">
-        <p> Cart Page</p>   
-        <SignOut/>
-
+  return (
+    <div className="cart-page">
+      <h1>My Carts</h1>
+      
+      {/* Wrapper for controlling padding of cart-list and button */}
+      <div className="cart-content-wrapper">
+        <div className="cart-list">
+          {carts.map((cart, index) => (
+            <CartItem
+              key={index}
+              title={cart.title}
+              paymentType={cart.paymentType}
+              paymentDue={cart.paymentDue}
+            />
+          ))}
+        </div>
+        <div className="create-cart-container">
+          <button className="create-cart-button">+ Create Cart</button>
+        </div>
       </div>
-    );
-  };
-  
-  export default CartPage;
-  
+    </div>
+  );
+};
+
+export default CartPage;
