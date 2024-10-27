ecab6dad144c81ae7bcff3cd32623c3f2a5daed4
Diana Whealan <dianawhealan@Dianas-MacBook-Air.local>
2024-10-27 08:57:50 -0500

    adding dropdowns

--- src/Pages/GoShoppingPage.jsx
+++ src/Pages/GoShoppingPage.jsx
@@ -1,13 +1,36 @@
 import { useNavigate } from 'react-router-dom';
-import { OrangeButton } from "../Components/Buttons"
-
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
+import { OptionDropdown, OrangeButton } from "../Components/Buttons"
+import './GoShoppingPage.css'
+const destinationOptions = ["Jewel", "Whole Foods", "Trader Joe's"];
+const cartOptions = ["Roommates", "Spring Break Trip", "Club Meeting"];
 const GoShoppingPage = () => {
+    const [destination, setDestination] = useState("Select Destination");
+    const [cart, setCart] = useState("Select Cart");
+
     const navigate = useNavigate();
     const DirectCheckList = () => {
         navigate('/'); // TODO: navigate to the checklist page, pass data needed
       };
       return(
-        <OrangeButton title={"Go shopping"} onClick={DirectCheckList}/>
+        <div className="center-container">
+            <Card className="custom-card" style={{ width: '18rem' }}>
+                <Card.Header className="custom-header text-center">Trip Details</Card.Header>
+                <Card.Body>
+                    <div className="mb-3">
+                        <strong>Destination:</strong>
+                        <OptionDropdown title={destination} onSelect={setDestination} options={destinationOptions} />
+                    </div>
+                    <div className="mb-3">
+                        <strong>Cart:</strong>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <OrangeButton title={"Go shopping"} onClick={DirectCheckList}/>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
       );
     
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,24 @@
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import "./Buttons.css"
 
 export const OrangeButton = ({onClick, title}) => (
     <Button className="orange-button" onClick={onClick}>{title}</Button>
 );
+
+export const OptionDropdown = ({title, onSelect, options}) => {
+    console.log(options);
+    return(
+    <DropdownButton
+        variant="secondary"
+        title={title}
+        className="ms-2"
+        onSelect={(e) => onSelect(e)}
+    >
+        {options.map((option, index) => (
+            <Dropdown.Item key={index} eventKey={option}>
+                {option}
+            </Dropdown.Item>
+        ))}
+    </DropdownButton>
+    );
+}
